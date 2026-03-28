54f039cf2bbacf7c2ca3f51be129570fd79d35a1
Edward Fernando Perez Gonzalez <ia.pg101524@ufg.edu.sv>
2026-03-28 11:18:48 -0600

    Inicio de proyecto tripsAN

---
+++ src/main/java/com/trips/Trips2026Application.java
@@ -0,0 +1,13 @@
+package com.trips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trips2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trips2026Application.class, args);
+	}
+
+}
